42ae1c93f9ca5eadb937d75257e0cd81c340e76c
lutze <lutze@192.168.0.142>
2012-11-02 20:06:30 +0100

    lab1 created

---
+++ lab1/McTaskOne/src/edu/mc/taskOne/Book.java
@@ -0,0 +1,61 @@
+package edu.mc.taskOne;
+
+public class Book {
+
+	private String author;
+	private String title;
+	private int price;
+	private String isbn;
+	private String course;
+	
+	public Book(String author, String title, int price, String isbn, String course) {
+		this.author = author;
+		this.title = title;
+		this.price = price;
+		this.isbn = isbn;
+		this.course = course;
+	}
+	
+	public Book() {
+		this.author = "unknown";
+		this.title = "unknown";
+		this.price = 0;
+		this.isbn = "0";
+		this.course = "unknown";
+	}
+	
+	// getter and setter methods
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+	
+	
+}
---
+++ lab1/McTaskOne/src/edu/mc/taskOne/TestActivity.java
@@ -0,0 +1,20 @@
+package edu.mc.taskOne;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class TestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		SimpleBookManager manager = new SimpleBookManager();
+		
+		for (int i=0;i<manager.count();i++)
+			Log.e("TEST", manager.getBookString(i));
+		
+	}
+	
+}
