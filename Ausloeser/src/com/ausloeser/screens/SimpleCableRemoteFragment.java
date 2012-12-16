59b2860b74588643a5c2a2ce3b529ffaf7ef52f1
lutze <lutze@192.168.1.101>
2012-12-16 15:06:16 +0100

    added disabling feature on exposure settings

--- Ausloeser/src/com/ausloeser/screens/SimpleCableRemoteFragment.java
+++ Ausloeser/src/com/ausloeser/screens/SimpleCableRemoteFragment.java
@@ -9,6 +9,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.SeekBar;
+import android.widget.ToggleButton;
 
 public class SimpleCableRemoteFragment extends SherlockFragment {
 
@@ -17,6 +22,21 @@ public class SimpleCableRemoteFragment extends SherlockFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_simple_cable_remote, container, false);
 	    
+		final ToggleButton buttonExposure = (ToggleButton) view.findViewById(R.id.ButtonExposure);
+		final SeekBar sliderExposure = (SeekBar) view.findViewById(R.id.SliderExposure);
+		final Button buttonExposureSelect = (Button) view.findViewById(R.id.ButtonExposureSelect);
+		
+		
+		buttonExposure.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+				sliderExposure.setEnabled(arg1);
+				buttonExposureSelect.setEnabled(arg1);
+			}
+			
+		});
+		
 		// apply fonts
 		Utils.applyFonts(view.findViewById(R.id.mainLayout),Typeface.createFromAsset(getActivity().getAssets(),"fonts/eurostile.ttf"));
 
