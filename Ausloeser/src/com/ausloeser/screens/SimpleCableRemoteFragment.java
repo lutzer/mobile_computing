260354f797f523128fe798eeff664c204006abdf
arnim <jepsen@gmx.de>
2012-12-16 17:01:00 +0100

    _ Arnim Button sollte triggerbar sein (ohne exposureAuswahl, einfach so)

--- Ausloeser/src/com/ausloeser/screens/SimpleCableRemoteFragment.java
+++ Ausloeser/src/com/ausloeser/screens/SimpleCableRemoteFragment.java
@@ -1,13 +1,10 @@
 package com.ausloeser.screens;
 
-import com.actionbarsherlock.app.SherlockFragment;
-import com.actionbarsherlock.view.Menu;
-import com.ausloeser.views.Utils;
-
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -15,7 +12,17 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.SeekBar;
 import android.widget.ToggleButton;
 
-public class SimpleCableRemoteFragment extends SherlockFragment {
+import com.actionbarsherlock.app.SherlockFragment;
+import com.ausloeser.logic.SingletonCameraController;
+import com.ausloeser.views.Utils;
+
+/**
+ * Fragment holding the interface to do a simple trigger
+ * 
+ * @author Arnim Jepsen & Lutz Reiter
+ *
+ */
+public class SimpleCableRemoteFragment extends SherlockFragment implements OnClickListener{
 
 
 	@Override
@@ -25,7 +32,9 @@ public class SimpleCableRemoteFragment extends SherlockFragment {
 		final ToggleButton buttonExposure = (ToggleButton) view.findViewById(R.id.ButtonExposure);
 		final SeekBar sliderExposure = (SeekBar) view.findViewById(R.id.SliderExposure);
 		final Button buttonExposureSelect = (Button) view.findViewById(R.id.ButtonExposureSelect);
-		
+		final Button buttonTrigger = (Button) view.findViewById(R.id.triggerButton);
+
+		buttonTrigger.setOnClickListener(this);
 		
 		buttonExposure.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
@@ -42,5 +51,18 @@ public class SimpleCableRemoteFragment extends SherlockFragment {
 
 
 		return view;
+	}
+	
+	@Override
+	public void onClick(View v) {
+		
+		switch( v.getId() ){
+		
+		case R.id.triggerButton:
+			SingletonCameraController.INSTANCE.triggerSimple();
+			break;
+		
+		}
+		
 	}
 }
