2312efa058a2ef676369ebc8b55a62cd18984e06
lutze <lutze@dhcp-194057.eduroam.chalmers.se>
2012-12-20 12:41:31 +0100

    fickscheisse

---
+++ Ausloeser/src/tools/SmsBroadcaster.java
@@ -0,0 +1,34 @@
+package tools;
+
+import java.util.ArrayList;
+
+import com.ausloeser.logic.OnDelayExposureTimerListener;
+
+enum SmsBroadcaster {
+	INSTANCE;
+	
+	
+	
+	public interface OnSmsReceivedListener {
+		void onSmsReceived(String sender, String msg);
+	}
+	
+	ArrayList<OnSmsReceivedListener> listeners = new ArrayList<OnSmsReceivedListener>();
+
+	
+	public void receivedSms(String sender, String msg) {
+		Log.e("INFO","received sms in broadcaster");
+		for(OnSmsReceivedListener listener:listeners){
+			listener.onSmsReceived(sender, msg);
+		}
+	}
+	
+	public void setOnSmsReceivedListener(OnSmsReceivedListener listener){
+		this.listeners.add(listener);
+	}
+	
+	public void removeOnSmsReceivedListener(OnSmsReceivedListener listener){
+		this.listeners.remove(listener);
+	}
+	
+}
